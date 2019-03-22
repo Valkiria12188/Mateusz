5e14813dbef734e64c18ec83ea2768bac9ba8154
Mateusz Proc <44548853+Valkiria12188@users.noreply.github.com>
2019-03-22 18:57:07 +0100

    Add files via upload

---
+++ zadanie 2 prog2.cpp
@@ -0,0 +1,128 @@
+﻿
+#include "pch.h"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Complex
+{
+public:
+	Complex(string);
+	string ConvertFromSymbolic();
+
+private:
+	string  Number;
+	string  Multiplier;
+
+};
+
+
+Complex::Complex(string  _Number )
+{
+	Number = _Number;
+	
+}
+
+string Complex::ConvertFromSymbolic() {
+	std::string s = "";
+	std::string Multiplier = "";
+	int CountZero = 0;
+	for (int i = 0; i < Number.length(); i++)
+	{
+		switch (Number[i])
+		{
+		case'0':
+		case'1':
+		case'2':
+		case'3':
+		case'4':
+		case'5':
+		case'6':
+		case'7':
+		case'8':
+		case'9':
+		case'.':
+			s += Number[i];
+			break;
+
+		default:
+
+			Multiplier += Number[i];
+			CountZero--;
+			break;
+		}
+		if (CountZero > 0 || Number[i] == '.')
+		{
+			CountZero++;
+		}
+	}
+	
+	
+	if (Multiplier == "M")
+	{
+		s += " 000 000";
+
+	}
+	else if (Multiplier == "B")
+	{
+
+		s += " 000 000 000";
+
+	}
+	else if (Multiplier == "T")
+	{
+
+		s += " 000 000 000 000";
+
+	}
+	else if (Multiplier == "Qa")
+	{
+
+		s += " 000 000 000 000 000";
+
+	}
+	else if (Multiplier == "Qi")
+	{
+
+		s += " 000 000 000 000 000 000";
+
+	}
+	else if (Multiplier == "Sx")
+	{
+
+		s += " 000 000 000 000 000 000 000";
+
+	}
+	else if (Multiplier == "Sp")
+	{
+
+		s += " 000 000 000 000 000 000 000 000";
+
+	}
+	else if (Multiplier == "0c")
+	{
+
+		s += " 000 000 000 000 000 000 000 000 000";
+
+	}
+	if (CountZero > 0)
+	{
+		s = s.substr(0, s.length() - (CountZero - 1));
+
+		int a = s.find(".");
+		if (a >= 0) s.erase(a, 1);
+	}
+	return s;
+}
+
+int main()
+{
+	Complex firstObject("9.22M");
+	Complex secondComplexObject("4B");
+	Complex thirdComplex("6.14T");
+	cout << firstObject.ConvertFromSymbolic() << endl;
+	cout << secondComplexObject.ConvertFromSymbolic() << endl;
+	cout << thirdComplex.ConvertFromSymbolic() << endl;
+}
+
